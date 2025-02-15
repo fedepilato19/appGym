e994ab8097a5af6f1415dc265d65a3e75d2304be
fedepilato19 <federicapilato1@gmail.com>
2025-02-15 13:39:35 +0100

    caricamento progressi codice esercizi

--- tempCodeRunnerFile.js
+++ tempCodeRunnerFile.js
@@ -1,1 +1,11 @@
-// Middleware per gestire il corpo delle richieste JSON
+//Funzione per ottenere gli esercizi dal database
+app.get('/getSavedExercises', (req, res) => {
+    const query = 'SELECT * FROM piano_workout';  // Modifica la query per recuperare gli esercizi
+    db.query(query, (err, result) => {
+        if (err) {
+            console.error('Errore durante il recupero degli esercizi:', err);
+            return res.status(500).send('Errore del server');
+        }
+        return res.json({status: true, result});
+    });
+});
